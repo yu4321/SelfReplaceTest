5dd3b4957c474bc4836516ff9f96cc4ff427f553
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2019-08-19 17:34:00 +0900

    초기 커밋.

---
+++ SelfReplaceTest/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SelfReplaceTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int lastVersion = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists("UpdateHistory.txt"))
+            {
+                string[] datas = File.ReadAllLines("UpdateHistory.txt");
+                lastVersion = int.Parse(datas[0]);
+            }
+            else
+            {
+                btn.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("UpdateNotifier.txt"))
+            {
+                string[] datas = File.ReadAllLines("UpdateNotifier.txt");
+                if (int.Parse(datas[0]) > lastVersion)
+                {
+                    if (datas.Length > 2)
+                    {
+                        Util.DirectoryCopyWithRenameFile(datas[1], datas[2], true);
+                    }
+                    else
+                    {
+                        Util.DirectoryCopyWithRenameFile(datas[1], Directory.GetCurrentDirectory(), true);
+                    }
+                    File.WriteAllText("UpdateHistory.txt", datas[0]);
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    MessageBox.Show($"최신 버전 {datas[0]} 현재 버전 {lastVersion}\n업데이트 불필요");
+                }
+            }
+        }
+    }
+}
---
+++ SelfReplaceTest/Util.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfReplaceTest
+{
+    public class Util
+    {
+        public static void DirectoryCopyWithRenameFile(string sourceDirName, string destDirName,
+                                      bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                for (int i = 0; i < 100; i++)
+                {
+                    string backupName = temppath + DateTime.Now.ToString("yyMMdd") + i.ToString();
+                    if (File.Exists(backupName))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        File.Move(temppath, backupName);
+                        break;
+                    }
+                }
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopyWithRenameFile(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
